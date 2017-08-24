94c37c00a6844a9a1711a3148e81fd6503e9c957
Cameron <marshacb@Camerons-MacBook-Pro.local>
2017-08-23 23:11:35 -0400

    users can see survey results

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,17 @@
+// SurveyField contains logic to render a single
+// label and text input
+import React from 'react';
+
+export default ({ input, label, meta: { error, touched } }) => {
+  return (
+    <div>
+      <label>
+        {label}
+      </label>
+      <input style={{ marginBottom: '5px' }} {...input} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
+    </div>
+  );
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   //auth piece of state is being produced by the auth reducer
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm,
+  surveys: surveysReducer
 });
